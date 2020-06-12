5c11c2e2df8f6eda7b0e4f02c860b3d64b484267
Kamil Zyla <7kamilzyla@gmail.com>
2020-06-12 17:44:23 +0200

    Map and simple module for vessel choice.

--- app.R
+++ app.R
@@ -1,25 +1,55 @@
 library(shiny)
+library(leaflet)
 library(data.table)
 
-cols <- c(
-  "SHIP_ID" = "factor",
-  "SHIPNAME" = "factor",
-  "ship_type" = "factor",
-  "DATETIME" = "character",
-  "LAT" = "numeric",
-  "LON" = "numeric"
+ships <- fread(
+  file = "ships.csv",
+  select = c(
+    "SHIP_ID" = "factor",
+    "SHIPNAME" = "factor",
+    "ship_type" = "factor",
+    "DATETIME" = "character",
+    "LAT" = "numeric",
+    "LON" = "numeric"
+  ),
+  col.names = c("id", "name", "type", "datetime", "lat", "lon")
 )
-rename <- c("id", "name", "type", "datetime", "lat", "lon")
-ships <- fread(file = "ships.csv", select = cols, col.names = rename)
 
-vessel_types <- levels(ships$type)
+vesselInput <- function(id) {
+  ns <- NS(id)
+  tagList(
+    selectInput(ns("type"), "Vessel type", levels(ships$type)),
+    selectInput(ns("name"), "Vessel name", levels(ships$name))
+  )
+}
+
+vessel <- function(input, output, session) {
+  reactive({
+    paste(input$type, input$name)
+  })
+}
 
 ui <- fluidPage(
-  selectInput("vesselType", "Vessel type", vessel_types),
+  titlePanel('Vessel App'),
+  sidebarLayout(
+    sidebarPanel(
+      vesselInput('vessel')
+    ),
+    mainPanel(
+      leafletOutput("map"),
+      textOutput("text")
+    )
+  )
 )
 
-server <- function(input, output, session) {
-  
+server <- function(input, output) {
+  vesselId <- callModule(vessel, 'vessel')
+  output$text <- renderText({
+    vesselId()
+  })
+  output$map <- renderLeaflet({
+    leaflet() %>% addProviderTiles(providers$OpenStreetMap.Mapnik)
+  })
 }
 
 shinyApp(ui, server)
