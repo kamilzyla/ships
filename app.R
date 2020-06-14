834670a4fe457d4e36bbad06aae03471c2908510
Kamil Zyla <7kamilzyla@gmail.com>
2020-06-14 12:01:53 +0200

    Finish and deploy.

--- app.R
+++ app.R
@@ -6,19 +6,23 @@ source("loadShips.R")
 vesselInput <- function(id) {
   ns <- NS(id)
   tagList(
-    selectInput(ns("type"), "Vessel type", levels(ships$type)),
+    selectInput(ns("type"), "Vessel type", NULL),
     selectInput(ns("name"), "Vessel name", NULL)
   )
 }
 
-vessel <- function(input, output, session) {
+vessel <- function(input, output, session, vessels) {
   observeEvent(input$type, {
-    choices = ships %>% filter(type == input$type) %>% select(name)
+    updateSelectInput(session, "type", choices = levels(vessels$type))
+  }, once = TRUE)
+  observeEvent(input$type, {
+    choices <- vessels %>% filter(type == input$type) %>% select(name)
     updateSelectInput(session, "name", choices = choices)
+  }, ignoreInit = TRUE)
+  row <- reactive({
+    vessels %>% filter(type == input$type, name == input$name)
   })
-  reactive({
-    c(type = input$type, name = input$name)
-  })
+  return(row)
 }
 
 ui <- fluidPage(
@@ -30,19 +34,22 @@ ui <- fluidPage(
     mainPanel(
       leafletOutput("map"),
       "Longest distance between observations: ",
-      textOutput("text", inline = TRUE)
+      textOutput("dist", inline = TRUE), "m."
     )
   )
 )
 
 server <- function(input, output) {
-  v <- callModule(vessel, 'vessel')
-  output$text <- renderText({
-    v <- v()
-    filter(ships, type == v[['type']], name == v[['name']])$dist
+  vesselRow <- callModule(vessel, 'vessel', ships)
+  output$dist <- renderText({
+    vesselRow()$dist
   })
   output$map <- renderLeaflet({
-    leaflet() %>% addProviderTiles(providers$OpenStreetMap.Mapnik)
+    r <- vesselRow()
+    opt <- labelOptions(textOnly = TRUE, noHide = TRUE)
+    leaflet() %>% addTiles() %>%
+      addMarkers(lat = r$lat1, lng = r$lon1, label = "Start", labelOptions = opt) %>%
+      addMarkers(lat = r$lat2, lng = r$lon2, label = "End", labelOptions = opt)
   })
 }
 
